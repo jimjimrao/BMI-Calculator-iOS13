b71125f156dff8d544bd5149545fa524a3bc5b8e
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-20 16:38:00 -0500

    show color and advice when calculating BMI

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -10,7 +10,10 @@ import UIKit
 
 class ResultViewController: UIViewController {
     var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
     
+    @IBOutlet weak var bgColor: UIImageView!
     @IBOutlet weak var bmiLabel: UILabel!
     @IBOutlet weak var adviceLabel: UILabel!
     
@@ -18,6 +21,8 @@ class ResultViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        bgColor.backgroundColor = color
     }
     @IBAction func recalculatePressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
--- BMI Calculator/Controllers/calculatorBrain.swift
+++ BMI Calculator/Controllers/calculatorBrain.swift
@@ -15,20 +15,26 @@ struct CalculatorBrain {
     
     mutating func calculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / (pow(height, 2))
-        bmi = BMI(value: bmiValue, advice: "", color: .systemBlue)
-        if let bmi = bmi {
-            if bmi.value < 18.5 {
-                print("Underweight")
-            } else if bmi.value >= 18.5 && bmi.value <= 24.9 {
-                print("Normal")
-            } else {
-                print("Overweight")
-            }
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more food", color: .systemBlue)
+        } else if bmiValue >= 18.5 && bmiValue <= 24.9 {
+            bmi = BMI(value: bmiValue, advice: "You are fit", color: .systemGreen)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Stop eating fatty", color: .systemRed)
         }
     }
     
     func getBMIValue() -> String {
         let bmiValue = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiValue
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? .systemGray
+    }
+    
+    func getAdvice() -> String{
+        return bmi?.advice ?? "Error Calculation"
     }
 }
